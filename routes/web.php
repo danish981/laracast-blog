1ee3cb87234037468979ecac45d6f2c0e68a55e1
Danish Mehmood <danishmehmood981@gmail.com>
2021-12-30 15:33:23 +0500

    eager load the relationships on existing models

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', static function () {
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->get()
     ]);
 });
 
