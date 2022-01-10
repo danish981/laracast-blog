0c3cc7542d8ab2d13d822fa58551ee8cd5be97f5
danish mehmood <danishmehmood981@gmail.com>
2022-01-10 18:11:31 +0500

    feel the power of combining the filters

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-
-// Route::get('categories/{category:slug}', static function (Category $category) {
-//     return view('posts', [
-//         'posts' => $category->posts,
-//         'currentCategory' => $category,
-//         'categories' => Category::all()
-//     ]);
-// })->name('category');
-
 Route::get('authors/{author:username}', static function (User $author) {
     return view('posts', [
         'posts' => $author->posts,
